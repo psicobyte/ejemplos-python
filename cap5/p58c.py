712a3a7850433ac3d202d0eb34905943e686b3e2
psicobyte <psicobyte@gmail.com>
2016-02-28 11:32:42 +0100

    más errores de indentación

--- cap5/p58c.py
+++ cap5/p58c.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-cadena_con_comillas = 'el signo "comillas" (") se usa en
-Python para acotar cadenas'
+cadena_con_comillas = 'el signo "comillas" (") se usa en Python para acotar cadenas'
--- cap5/p58d.py
+++ cap5/p58d.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-cadena_con_comillas = "el signo \"comillas\" (\") se usa en
-Python para acotar cadenas de texto"
+cadena_con_comillas = "el signo \"comillas\" (\") se usa en Python para acotar cadenas de texto"
