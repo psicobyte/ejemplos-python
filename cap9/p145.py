627901b5baa8b55af368a82f68a1b655f305dfc7
psicobyte <psicobyte@gmail.com>
2016-03-07 20:29:36 +0100

    Comenzamos capítulo 9

---
+++ cap9/p145.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# mates.py es un módulo de ejemplo con operaciones matemáticas
+
+variable = "Esto está en el módulo"
+
+def multiplica(num1, num2):
+    """ Función que multiplica dos números """
+    return num1 * num2
+
+def suma(num1, num2):
+    """ Función que suma dos números """
+    return num1 + num2
